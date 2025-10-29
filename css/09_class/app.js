9969953125ef5025acb58315016e3f014dd87f2e
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-10-29 21:53:02 +0500

    chore: class 9 added

---
+++ css/09_class/app.js
@@ -0,0 +1,84 @@
+// alert("hello world");
+
+// alert("Thanks for your input!");
+
+// variables
+
+// var 
+// let
+// const
+
+var name; // initialization
+name = "John"; //declaration / assignment
+
+// shorthand
+
+var name = "Mohtashim";
+// console.log(name);
+
+
+
+var age = 21;
+// console.log(age);
+
+
+var thanx = "Thanks for your input!"
+// alert(thanx);
+
+var firstno = 10;
+var secondno = 20;
+var sum = firstno + secondno;
+// sum = 10 + 20 = 30
+// console.log(sum);
+// console.log("sum");
+
+// var f = 30
+// console.log(f + 10);
+
+
+var int = 10; // integer
+var float = 10.5 // float
+
+
+var $2number = 30;
+// console.log(n + 10);
+
+
+
+var fatherName1234$ 
+
+
+var Rose = "123";
+// console.log(rose);
+
+
+
+var calculation2 = 10 / 20;
+// console.log(calculation2);
+
+var calculation1 = 20 * 10;
+// console.log(calculation1);
+
+
+
+
+var num = 10;
+// console.log(--num);
+
+
+var calc = (2 + 2) * (10 / 3);
+// console.log(calc);
+
+
+var userName = "Ali";
+// alert("Hello" + " " + userName);
+
+
+name2 = "Ali";
+var name1 = prompt("What is your name?", `${name2}`);
+console.log(name1);
+
+
+
+
+
